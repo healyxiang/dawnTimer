11ddb25cea21cc52fc1adf3bf0637cebb6b2b86d
healy <xianglitm@163.com>
2025-05-14 22:45:43 +0800

    feat: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next";
+// import { allBlogs } from "contentlayer/generated";
+// import { allPosts, allDePosts, allCsPosts } from "contentlayer/generated";
+import { allPosts } from "contentlayer/generated";
+
+import siteMetadata from "@/data/siteMetadata";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = siteMetadata.siteUrl;
+  const blogRoutes = allPosts.map((post) => ({
+    url: `${siteUrl}/${post.url}`,
+    lastModified: post.date,
+  }));
+
+  const routes = ["", "pomodoro", "blog"].map((route) => ({
+    url: `${siteUrl}/${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
+
+  return [...routes, ...blogRoutes];
+}
