7092a33450bd3afd10950fd1dfe3c3fd8d675d41
healy <xianglitm@163.com>
2025-06-25 23:33:16 +0800

    feat: add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,7 +12,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     lastModified: post.date,
   }));
 
-  const routes = ["", "pomodoro", "blog"].map((route) => ({
+  const routes = ["", "pomodoro", "taskmatrix", "blog"].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
